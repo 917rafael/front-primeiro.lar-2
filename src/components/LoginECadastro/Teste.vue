955a3b8e4806730d2988f6d3bcf8429839f168b4
lucas <lucasdesouzaosouza57@gmail.com>
2025-10-16 11:30:14 -0300

    Feat: Testando ligação com o Backend

---
+++ src/components/LoginECadastro/Teste.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Lista de Usuários</h1>
+    <ul>
+      <li v-for="usuario in usuarios" :key="usuario.id">
+        {{ usuario.nome }} - {{ usuario.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import api from "../services/api";
+
+const usuarios = ref([]);
+
+onMounted(async () => {
+  const response = await api.get("usuarios/");
+  usuarios.value = response.data;
+});
+</script>
